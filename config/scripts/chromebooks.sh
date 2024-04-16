d21127c7ebeaa11cf67018b7f83436527f2dcf7a
birralee-ict <162308039+birralee-ict@users.noreply.github.com>
2024-04-16 13:17:28 +0200

    feat: replace ppd with tuned

--- config/scripts/chromebooks.sh
+++ config/scripts/chromebooks.sh
@@ -5,12 +5,27 @@ set -euo pipefail
 rpm-ostree override remove \
     podman \
     distrobox \
-    toolbox \
+    toolbox && \
+rpm-ostree override remove \
     krfb \
     krfb-libs \
     kde-connect \
     kde-connect-libs \
-    kdeconnectd
+    kdeconnectd && \
+rpm-ostree override remove \
+    power-profiles-daemon \
+    || true && \
+rpm-ostree override remove \
+    tlp \
+    tlp-rdw \
+    || true && \
+rpm-ostree install \
+    tuned \
+    tuned-ppd \
+    tuned-utils \
+    tuned-gtk \
+    tuned-profiles-atomic \
+    tuned-profiles-cpu-partitioning
 
 # Enable student user generation
 systemctl enable student-user.service
