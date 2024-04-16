62368f39363ba975cd20a9a3f98d3f9899499bb6
birralee-ict <162308039+birralee-ict@users.noreply.github.com>
2024-04-16 09:32:59 +0200

    feat: remove goverlay, lutris and steam from apps to uninstall due to move to aurora

--- config/scripts/main.sh
+++ config/scripts/main.sh
@@ -3,9 +3,6 @@ set -euo pipefail
 
 # Remove things we do not want on the image
 rpm-ostree override remove \
-    steam \
-    lutris \
-    goverlay \
     krfb \
     krfb-libs \
     kde-connect \
