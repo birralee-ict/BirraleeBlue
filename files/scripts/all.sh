980872ee1fb8453331ee23d9c70dcf3fd9552037
birralee-ict <162308039+birralee-ict@users.noreply.github.com>
2025-01-17 11:22:28 +0100

    feat: add working flatpak auto update to all images

--- files/scripts/all.sh
+++ files/scripts/all.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 # Enable ntp time server sync
 systemctl enable systemd-timesyncd.service
 
+# Enable flatpak auto updater
+systemctl enable flatpak-update.timer
+
 # Make sure image is signed
 systemctl enable birralee-sign-image.service
